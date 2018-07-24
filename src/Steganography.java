287cc2c3d5dce4113780625332c41c4a0ff65ef9
Paulo Amorim <pamorim@avenuecode.com>
2018-07-24 17:17:26 -0300

    1 - Get bits of Image 2 - Get bits of message 3 - Changing least significant bits in image(last 2) with the bits of message

--- src/Steganography.java
+++ src/Steganography.java
@@ -2,23 +2,85 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Steganography {
+	
+	private static String message = "Test enconding to binary";
 
 	public static void main(String[] args) throws IOException {
-		//File image = new File("download.png");
 		Path image = Paths.get("download.png");
 		byte[] bytes = Files.readAllBytes(image);
 		
+		List<String> listImage = new ArrayList<>();
+		List<Character> listMessage = new ArrayList<>();
 		
-		for (byte b : bytes) {
-			String bitRepresentation  = toBitString(b);
-			System.out.println(b+"->  "+bitRepresentation 
-					+ " -> length: "+bitRepresentation.length());
-			
+		//adding \q to know where I should stop on decoding
+		message = message.concat("\\q");
+				
+		convertingBytesOfImageToBits(bytes, listImage);
+		convertingMessageToListOfCharacter(listMessage);
+		changeLeastSignificantBits(listImage, listMessage);
+		
+		//creating another image with the updated list of bits
+		byte[] newImage = new byte[listImage.size()];
+		for(int i = 0; i < listImage.size();i++) {
+			newImage[i] = Byte.parseByte(listImage.get(i));
+		}
+		
+		
+	}
+	/**
+	 * Iterate over List<Image>, for each sequence of 8 bits
+	 * Get the least significant bits, in this case the last 2
+	 * change the value with the bit of message we want to hide
+	 * @param listImage
+	 * @param listMessage
+	 */
+	private static void changeLeastSignificantBits(List<String> listImage, List<Character> listMessage) {
+		int countMessage=0;
+		for (int i = 0; i < listImage.size();i++) {
+			if(countMessage + 1 < listMessage.size()) {
+				String bits = listImage.get(i);
+				bits = bits.substring(0, bits.length()-2)
+						+listMessage.get(countMessage)
+						+listMessage.get(++countMessage);
+				countMessage++;
+				listImage.set(i,bits);
+			}else {
+				break;
+			}
 		}
 	}
-	public static String toBitString(final byte val) {
+	/**
+	 * Get the bytes of message, convert into a String representation of bits. Add these bits
+	 * in a List<Character>
+	 * @param listMessage
+	 */
+	private static void convertingMessageToListOfCharacter(List<Character> listMessage) {
+		for(byte b : message.getBytes()) {
+			for(char c : toBitString(b).toCharArray()) {
+				listMessage.add(c);
+			}
+		}
+	}
+	/**
+	 * Converting the bytes into a String representation of bits value.
+	 * @param bytes
+	 * @param listImage
+	 */
+	private static void convertingBytesOfImageToBits(byte[] bytes, List<String> listImage) {
+		for (byte b : bytes) {
+			 listImage.add(toBitString(b));
+		}
+	}
+	/**
+	 * Converting byte in String representation of bits
+	 * @param val
+	 * @return
+	 */
+	private static String toBitString(final byte val) {
 		return String.format("%8s", Integer.toBinaryString(val & 0xFF))
 	               .replace(' ', '0');
 	}		
